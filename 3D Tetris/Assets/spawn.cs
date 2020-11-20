5fe0998410611518a08eae391641c1e7589de483
Connor Hulla <hullaconnor@gmail.com>
2020-11-20 16:08:27 -0600

    7 bag randomization
    7 bag has been implemented

--- 3D Tetris/Assets/spawn.cs
+++ 3D Tetris/Assets/spawn.cs
@@ -6,15 +6,40 @@ public class spawn : MonoBehaviour
 {
 	public GameObject[] Tetrominoes;
     // Start is called before the first frame update
+	private static int bagCount;
     void Start()
     {
+		bagCount = 0;
+		Shuffle();
         NewTetromino();
     }
 
     // Update is called once per frame
     public void NewTetromino()
     {
-        Instantiate(Tetrominoes[Random.Range(0,Tetrominoes.Length)], transform.position, Quaternion.identity);
+		if(bagCount == 7) {
+			Shuffle();
+			bagCount = 0;
+		}
+		//spawn the tetramino at the location of the spawner
+        Instantiate(Tetrominoes[bagCount], transform.position, Quaternion.identity);
+		bagCount++;
     }
+	
+	//shuffle the list;
+	private void Shuffle() { 
+		int randomIndex;
+		
+		for (int i= 0; i < Tetrominoes.Length; i++) {
+			randomIndex = Random.Range(0, Tetrominoes.Length);
+			swap(i, randomIndex);
+		}
+	}
+	
+	private void swap(int i, int j) {
+		GameObject hold = Tetrominoes[j];
+		Tetrominoes[j] = Tetrominoes[i];
+		Tetrominoes[i] = hold;
+	}
 }
  
