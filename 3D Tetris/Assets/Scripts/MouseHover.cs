1403934290c2c32b9a9a5f9491b8a0b33298e9a4
Gemaris <gxv170430@utdallas.edu>
2020-11-22 18:45:23 -0600

    Edited ghost piece material and grid more

---
+++ 3D Tetris/Assets/Scripts/MouseHover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    //Mouse enters -> change text color to light blue
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = new Color(.561f,.863f,.992f);
+    }
+	
+	//Mouse exits -> change text color back to white
+	void OnMouseExit()
+	{
+		GetComponent<Renderer>().material.color = Color.white;
+	}
+}
